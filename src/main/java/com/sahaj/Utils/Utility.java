c88ddee382eb9a825d862df7f48af3055a61be5b
Narendar Singh <narendarsinghj@gmail.com>
2015-10-30 01:26:02 +0530

    Initial Commit

---
+++ src/main/java/com/sahaj/Utils/Utility.java
@@ -0,0 +1,22 @@
+package com.sahaj.Utils;
+
+/**
+ * Utility Class to have Utils methods.
+ * 
+ * @author Naren
+ *
+ */
+public class Utility
+{
+	/**
+	 * Validate whether the String is Null or Empty
+	 * 
+	 * @param value
+	 *            String value
+	 * @return true if value is null or emptys
+	 */
+	public static boolean isStringNullOrEmpty(String value)
+	{
+		return value == null || value.isEmpty();
+	}
+}
